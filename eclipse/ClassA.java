9169b75ad0c4a169726201a60106a3d32b9ae379
Aaditya Buchale <103892684+aadityabuchale@users.noreply.github.com>
2022-11-17 23:41:49 +0530

    Add files via upload

--- eclipse/ClassA.java
+++ eclipse/ClassA.java
@@ -1,8 +1,22 @@
-package basicprograms;
+// 16-01-2022 Homework 
+
+package homework;
 
 public class ClassA {
-	 public static void Main(String[] args)
-	{
-		System.out.println("Hello AB..");
+	int c;
+	static int e;
+	ClassA(){
+		System.out.println(++c);
+		System.out.println(++e);
+	}
+	public static void main(String[] args) {
+		int a=34;
+		int b=21;
+		new ClassA().c -= a++ + ++b;
+		int d = --a + --b + new ClassA().c--;
+		e=a + +b + +new ClassA().c +d--;
+		int f= -a +b-- + -new ClassA().c -d++;
+		int sum = a+b+new ClassA().c+d+e+f;
+		System.out.println("Sum ="+sum);
 	}
 }
